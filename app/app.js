48d8b6ac4137a4798148cdcb3f17d027766808eb
Perl <72554458+BenjMoore@users.noreply.github.com>
2023-12-19 16:20:21 +1000

    A

--- app/app.js
+++ app/app.js
@@ -1,14 +1,28 @@
+// set express value
 var express = require('express');
+
+// create express instance
 var app = express();
+
+// create path constant
 const path = require('path');
 
+// set view engine to ejs for partials
+app.set('view engine', 'ejs');
+
+// App Route / (Home)
+app.get('/', (request, response) => {
+    response.render('index', {
+      subject: 'Vertigo Robotics',
+      name: 'Home',
+      link: 'https://google.com'
+    });
+  });
 
 
-app.get('/', function (req, res) {
-  const filePath = path.resolve(__dirname, 'views/index.html');
-  res.sendFile(filePath);
-});
-
+// Start Application
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.clear();
+  console.log('Vertigo By Oasis Interactive\n[+] Running');
+
 });
